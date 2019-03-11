4c8fa0e495368cea075020542ed5a50ec7c8e5a3
Simon Thür <thuer.simon@hotmail.com>
2019-03-11 13:54:38 +0100

    Create Jassen.py
    Yep, literally created a file called Jassen.py

---
+++ Jassen.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Project:       Deep Jass
+
+File:           Jassen.py
+Purpose:        AI learning to play cards.
+
+Created on:     11.03.2019 13:41:18
+Author:         Simon Thür; Marc Matter
+
+Copyright © 2019
+"""
+
