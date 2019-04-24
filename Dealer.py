2d3c506cd673bb34a5731ba35703733faa62d493
Oktavlauch <46028262+Oktavlauch@users.noreply.github.com>
2019-04-24 16:47:50 +0200

    edited dealer.py

--- Dealer.py
+++ Dealer.py
@@ -14,7 +14,7 @@ Copyright © 2019 Marc Matter, Michael Siebenmann, Ramon Heeb, Simon Thür. All 
 
 
 import numpy as np
-import multiprocessing #*NOTE Multiprocessing ?usually? does not work in iPython (Spyder). To use MP, run file through Anaconda: navigate to folder and type: `python JassRNN.py`
+import multiprocessing #*NOTE Multiprocessing ?usually? does not work in iPython (Spyder). To use MP, run file through Anaconda: navigate to folder and type: `python Dealer.py`
 from tqdm import tqdm  #using anaconda/pip: pip install tqdm
 import JassRNN as rnn
 import Jassen as js
@@ -27,8 +27,23 @@ import Jassen as js
 # =============================================================================
 if __name__ == '__main__':
     Players = []
-    for i in range(4):
-        name = 'p' + i
-        Players.append(rnn.GetModel())
-        rnn.LoadWeights(Players[i], name)
+# =============================================================================
+#     for i in range(4):
+#         name = 'p' + str(i)
+#         Players.append(rnn.GetModel())
+#         rnn.LoadWeights(Players[i], name)
+# =============================================================================
+        
+    Cards = js.Shuffle()
+    Round = 0
+    
+    style = np.random.randint(6) #subject to change
+    Cards.append(style) #add trump
+    
+    
+    Round += 1
+    Round %= 4
+    
+    
+
         
