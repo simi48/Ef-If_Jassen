1ee6bc789ad23d7cc45299a5b78c69ec9def680d
Oktavlauch <46028262+Oktavlauch@users.noreply.github.com>
2019-04-30 22:11:48 +0200

    added Gamemaster, edited description of Dealer.py

---
+++ GameMaster.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Project:       Deep Jass
+
+File:           Gamemaster.py
+
+Purpose:        Functions which serve the purpose of managing a game between an AI and three players
+
+Created on:     30.04.2019 22:01:30
+Author:         Simon Thür; Marc Matter
+
+Copyright © 2019 Marc Matter, Michael Siebenmann, Ramon Heeb, Simon Thür. All rights reserved.
+"""
+
+def PlayGame():
+    pass
+
+
+
+
+# =============================================================================
+# Main
+# =============================================================================
+if __name__ == '__main__':
+    pass
