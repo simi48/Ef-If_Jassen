971bf07b268907706eb565f6ca745e452ab1ff17
Oktavlauch <marc.matter@gmx.ch>
2019-05-13 14:56:38 +0200

    edited example map, get values of the Card Recognition RNN
    Co-Authored-By: Michael <thesiebi@users.noreply.github.com>

--- android/app/src/main/java/org/tensorflow/lite/examples/detection/customview/RecognitionScoreView.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/customview/RecognitionScoreView.java
@@ -19,17 +19,70 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
+
+import java.util.ArrayList;
 import java.util.List;
 import org.tensorflow.lite.examples.detection.tflite.Classifier.Recognition;
 
+
 public class RecognitionScoreView extends View implements ResultsView {
+
+  public class CardRecog{ //array which is able to hold 36 cards with names attached to them
+    String CardTitle;
+    double Confidence;
+  }
+
   private static final float TEXT_SIZE_DIP = 14;
   private final float textSizePx;
   private final Paint fgPaint;
   private final Paint bgPaint;
   private List<Recognition> results;
+  private CardRecog[] tmpCards = new CardRecog[36];
+  public CardRecog[] myCards = fillCardNames(tmpCards);
+  private static final String TAG = "RecognitionActivity";
+
+
+  public CardRecog[] fillCardNames(CardRecog[] myCards){ //fill CardRecog with the names of the cards
+    String cardType = "Rosen ";
+    for(int i = 0; i < 36; i++){
+      if(i == 9){
+        cardType = "Eicheln ";
+      }
+      if(i == 18){
+        cardType = "Schellen ";
+      }
+      if(i == 27){
+        cardType = "Schilten ";
+      }
+
+      if(i % 9 < 6) {
+        myCards[i].CardTitle = cardType + Integer.toString(i + 6);
+        myCards[i].Confidence = 0;
+      }
+      else if(i % 9 == 6){
+        myCards[i].CardTitle = cardType + "Under";
+        myCards[i].Confidence = 0;
+      }
+      else if(i % 9 == 7){
+        myCards[i].CardTitle = cardType + "Ober";
+        myCards[i].Confidence = 0;
+      }
+      else if(i % 9 == 8){
+        myCards[i].CardTitle = cardType + "König";
+        myCards[i].Confidence = 0;
+      }
+      else if(i % 9 == 0){
+        myCards[i].CardTitle = cardType + "Ass";
+        myCards[i].Confidence = 0;
+      }
+    }
+    Log.d(TAG, "fillCardNames: " + myCards);
+    return myCards;
+  }
+
 
   public RecognitionScoreView(final Context context, final AttributeSet set) {
     super(context, set);
@@ -61,6 +114,14 @@ public class RecognitionScoreView extends View implements ResultsView {
       for (final Recognition recog : results) {
         canvas.drawText(recog.getTitle() + ": " + recog.getConfidence(), x, y, fgPaint);
         y += (int) (fgPaint.getTextSize() * 1.5f);
+
+        for (int i = 0; i < 36; i++){ //write Confidence of each recogniced card into the card array
+          if(myCards[i].CardTitle.equals(recog.getTitle())){
+            if(myCards[i].Confidence < recog.getConfidence()){
+              myCards[i].Confidence = recog.getConfidence();
+            }
+          }
+        }
       }
     }
   }
