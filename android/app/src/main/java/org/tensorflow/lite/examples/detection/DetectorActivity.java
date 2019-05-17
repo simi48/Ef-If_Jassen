c1a24ea6e0d5319d2ded3bb0bb397bea8c5d1617
TheSiebi <TheSiebi@users.noreply.github.com>
2019-05-18 00:53:21 +0200

    trying to plug in our fancy model without success *cry*

--- android/app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
@@ -65,9 +65,9 @@ public class DetectorActivity extends CameraActivity implements OnImageAvailable
 
   // Configuration values for the prepackaged SSD model.
   private static final int TF_OD_API_INPUT_SIZE = 300;
-  private static final boolean TF_OD_API_IS_QUANTIZED = true;
-  private static final String TF_OD_API_MODEL_FILE = "detect.tflite";
-  private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/labelmap.txt";
+  private static final boolean TF_OD_API_IS_QUANTIZED = false; // maybe also true, dunno yet
+  private static final String TF_OD_API_MODEL_FILE = "tiny-yolo-obj.lite";
+  private static final String TF_OD_API_LABELS_FILE = "labelmap2.txt";
   private static final DetectorMode MODE = DetectorMode.TF_OD_API;
   // Minimum detection confidence to track a detection.
   private static final float MINIMUM_CONFIDENCE_TF_OD_API = 0.5f;
