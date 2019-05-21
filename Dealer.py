79dd0fc476b8ebdb4b1948b0448fbac2561cb61f
simi48 <41296975+simi48@users.noreply.github.com>
2019-05-21 21:13:14 +0200

    Made a little error I think? or it doesnt work how I thought it would, trying again
    Just a small one...

--- Dealer.py
+++ Dealer.py
@@ -431,8 +431,10 @@ def MPTrain(model_list, generations = 100, epochs = 25000, batch = 10, mutations
             
             #save it usefully, also, lets test this a bit...
             rnn.pb_conversion(bestmodel)
-            rnn.pb_conversion(bestmodel,name=ctime())
-            rnn.pb_conversion(bestmodel,name=ctime(),text=True)
+            tmpname = ctime()
+            rnn.pb_conversion(bestmodel,name=tmpname)
+            tmpname = tmpname + ' _txt'
+            rnn.pb_conversion(bestmodel,name=tmpname,text=True)
             
             for table in range(processes):
                 model_list[table][3] = bestmodel
